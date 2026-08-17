e5347085cbbe17b6f91f54156c84856dae30d493
kyu <66315807+jhikyuinn@users.noreply.github.com>
2026-08-17 23:47:06 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 1/12934/정수 제곱근 판별.java
@@ -0,0 +1,7 @@
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+        if (Math.sqrt(n) % 1 == 0) answer = ((long)Math.sqrt(n)+1) * ((long)Math.sqrt(n)+1);
+        return answer;
+    }
+}
