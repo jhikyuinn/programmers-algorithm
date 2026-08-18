4c15397b3dc26b5c9dbd6ff8a84a248b2d2c3ddd
kyu <66315807+jhikyuinn@users.noreply.github.com>
2026-08-18 23:35:52 +0900

    [level 0] Title: 이진수 더하기, Time: 0.02 ms, Memory: 83 MB -BaekjoonHub

---
+++ 0/120885/이진수 더하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String bin1, String bin2) {
+        
+        int sum = Integer.parseInt(bin1,2) + Integer.parseInt(bin2,2);
+        String answer = Integer.toBinaryString(sum);
+        return answer;
+    }
+}
