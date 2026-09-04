b9b937c9d62ff16b82fd7f89b0768715d0a15ac6
kyu <66315807+jhikyuinn@users.noreply.github.com>
2026-09-05 01:32:54 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 0/120866/안전지대.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board) {
+        int n = board.length;
+        boolean[][] danger = new boolean[n][n];
+        
+        int[] dx = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    for (int d = 0; d < 9; d++) {
+                        int nx = i + dx[d];
+                        int ny = j + dy[d];
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                            danger[nx][ny] = true;
+                        }
+                    }
+                }
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!danger[i][j]) {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
